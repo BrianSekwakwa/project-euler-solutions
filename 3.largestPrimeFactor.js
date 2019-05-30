21ebfea5a44cc099f22ce5bc1b7f8acd6687edbd
Brian Sekwakwa <briansenpai14@gmail.com>
2019-05-30 16:08:44 +0200

    completed the largest prime factor problem

---
+++ 3.largestPrimeFactor.js
@@ -0,0 +1,35 @@
+// Problem
+
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+
+// Problem break down
+
+// Find all prime numbers from 10 up to the number n
+// Find how many of these prime numbers are factors of the number n
+// Within those factors that were found, find the largest number among them
+
+let primeNumbers = [2, 3, 5, 7];
+let primeFactors = [];
+
+function primeFactor(num) {
+  // Finding prime numbers up to n
+  for (let i = 10; i <= num; i++) {
+    if (i % 2 === 0 || i % 3 === 0 || i % 5 === 0 || i % 7 === 0) {
+    } else {
+      primeNumbers.push(i);
+    }
+  }
+
+  // Finding which of the prime numbers are factors of n
+  primeNumbers.forEach(prime => {
+    if (num % prime === 0) {
+      primeFactors.push(prime);
+    }
+  });
+
+  // Finding the larget prime factor of n
+  console.log(Math.max(...primeFactors));
+}
+
+primeFactor(13195);
