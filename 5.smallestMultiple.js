5cc62630329d1a892c61f84651c8dfbc2fe9397f
Brian Sekwakwa <briansenpai14@gmail.com>
2019-06-06 12:26:15 +0200

    completed the smallest multiple problem

--- 5.smallestMultiple.js
+++ 5.smallestMultiple.js
@@ -5,14 +5,31 @@
 
 // Problem Break Down
 
+// Divide each counter starting from 1 by numbers ranging from 1 to 10
+// check to see if the counter is evenly divisible by the range of number 1 to 10
+// continue to check until you find the smallest counter that can be evenly divisible by the numbers 1 to 10
+
 let isSmallestMultiple = false;
 let counter = 1;
+let evenlyDivisible = [];
+let result;
 
 function smallestMultiple() {
   while (isSmallestMultiple === false) {
-    for (let i = 1; i <= 10 ; i++) {
-
+    // Looping for the numbers 1 to 10 to divide the counter with
+    for (let i = 1; i <= 10; i++) {
+      result = counter % i;
+      // Checking to see if the counter gets evenly divided by (n) in the range 1 to 10
+      if (result === 0) {
+        evenlyDivisible.push(i);
+        if (evenlyDivisible.length === 10) {
+          isSmallestMultiple = true;
+        }
+      }
     }
+
+    evenlyDivisible = [];
+    counter++;
   }
 }
 
