eeaef0bad9ff12aeb1d434c7c586c5c415a72e00
Brian Sekwakwa <briansenpai14@gmail.com>
2019-06-12 17:53:02 +0200

    completed the pythogorean triplet problem

---
+++ 9.pythogoreanTriplet.js
@@ -0,0 +1,32 @@
+// Problem
+
+// A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+// a2 + b2 = c2
+
+// For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+// Find the product abc.
+
+// Problem Break Down
+
+function pythogoreanTriplet() {
+  let tripletSum = 1000;
+  let abSum;
+  let abcProduct;
+  let c;
+
+  for (let a = 1; a <= tripletSum; a++) {
+    for (let b = 1; b <= tripletSum; b++) {
+      c = tripletSum - a - b;
+      abSum = a ** 2 + b ** 2;
+      if (a < b && abSum === c ** 2) {
+        abcProduct = a * b * c;
+      }
+    }
+  }
+
+  console.log(`The product is ${abcProduct}`);
+}
+
+pythogoreanTriplet();
